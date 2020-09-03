4accbcfa8f808b7ee71c872b54a11ec0d5aa3d9d
Brian Sahota <bpsahota@gmail.com>
2020-09-03 17:08:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,7 @@ def monopoly_with_third_tier
   names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, short_line_railroad: {}
   }
   }
+  }
 end
 
 def monopoly_with_fourth_tier
